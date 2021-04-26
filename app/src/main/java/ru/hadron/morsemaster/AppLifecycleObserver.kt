edb466cb93eca6fab2d3e0c0f337cb7ac8f3ccd1
Katya Popova <hadron@yandex.ru>
2021-04-26 17:13:25 +0300

    -

--- app/src/main/java/ru/hadron/morsemaster/AppLifecycleObserver.kt
+++ app/src/main/java/ru/hadron/morsemaster/AppLifecycleObserver.kt
@@ -6,15 +6,14 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.OnLifecycleEvent
-import timber.log.Timber
 import javax.inject.Inject
 
 class AppLifecycleObserver @Inject constructor(
     context: Context
 ) : LifecycleObserver {
+
     companion object {
         var isMoveToBackground = false
-        var isMoveToForeBackground = true
     }
 
     private val enterForegroundToast =
@@ -30,25 +29,22 @@ class AppLifecycleObserver @Inject constructor(
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
-    fun onMoveToForeground(): Boolean {
+    fun onMoveToForeground() {
         enterForegroundToast.showAfterCanceling(enterBackgroundToast)
-        Timber.e(" app moved to foreground   //isMovedToForeground == ${isMovedToForeground.value}, ${isMoveToForeBackground}")
+
         isMovedToForeground.postValue(true)
-        isMoveToForeBackground = true
         isMoveToBackground = false
-        Timber.e(" app moved to foreground   //isMovedToForeground == ${isMovedToForeground.value}, ${isMoveToForeBackground}")
-        return isMoveToForeBackground
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun onMoveToBackground() {
         enterBackgroundToast.showAfterCanceling(enterForegroundToast)
-        Timber.e(" app moved to background /isMovedToForeground = ${isMovedToForeground.value}, ${isMoveToForeBackground}")
+
         isMoveToBackground = true
         isMovedToForeground.postValue(false)
-        isMoveToForeBackground = false
-        Timber.e(" app moved to background /isMovedToForeground = ${isMovedToForeground.value}, ${isMoveToForeBackground}")
+
     }
+
     private fun Toast.showAfterCanceling(toastToCancel: Toast) {
         toastToCancel.cancel()
         this.show()
